80c44fea671b636b71f730a4717ec7e1e4491090
jay shah <52785256+Jay5877@users.noreply.github.com>
2020-08-10 00:22:38 +0530

    Add files via upload

---
+++ Perceptron1.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+class Perceptron(object):
+    def __init__(self,no_of_inputs,threshold=100,learning_rate=.001):
+    
+        self.threshold=threshold
+        self.learning_rate=learning_rate
+        self.weights=np.zeros(no_of_inputs + 1)
+    
+    def predict(self,inputs):
+        summation=np.dot(inputs,self.weights[1:])+self.weights[0]           #w.x+b
+        if summation > 0:
+            activation = 1
+        else:
+            activation = 0
+        return activation
+    
+    def train(self,training_inputs,labels):
+        print("Threshold \t Prediction \t\t Weights")
+        print("=================================================================")
+       
+        for _ in range(self.threshold):
+            test = []
+            L = []
+            for inputs,label in zip(training_inputs,labels):
+                prediction=self.predict(inputs)
+                self.weights[1:]+=self.learning_rate*(label-prediction)*inputs
+                self.weights[0]+=self.learning_rate*(label-prediction)
+                test.append(prediction)
+                L.append(label)
+                print(_,end=" \t\t\t ")
+                print(prediction, end= " \t\t\t ")
+                print(self.weights)
+
+                if (len(test) == len(L)):
+                    count = 0
+                    if(test == L):
+                        count += 1
+            if(count>0):
+                break    
+                     
+        print("threshold stoped:",end= " " )
+        print(_)
+        print("=================================================================")
---
+++ Perceptron_Try2.py
@@ -0,0 +1,19 @@
+import numpy as np
+from Perceptron1 import Perceptron 
+
+training_inputs=[]
+training_inputs.append(np.array([1,1,1]))
+training_inputs.append(np.array([1,1,0]))
+training_inputs.append(np.array([1,0,0]))
+training_inputs.append(np.array([0,1,0]))
+training_inputs.append(np.array([0,1,1]))
+training_inputs.append(np.array([1,0,1]))
+
+labels = np.array([1,0,0,0,0,0])
+
+perceptron = Perceptron(3)
+perceptron.train(training_inputs,labels)
+
+inputs = np.array([0,0,1])
+print(perceptron.predict(inputs))
+print(perceptron.weights)
